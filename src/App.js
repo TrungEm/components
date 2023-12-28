673c4bf65cbd7e019cbf0f986a68d2bfff320235
Trung Em ^_^ <fullstack6015@gmail.com>
2023-12-28 07:24:05 +0700

    Test clsx

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-// import clsx from 'clsx';
+import clsx from 'clsx';
 
 function App() {
-    return <div className="App">{<h1 className="sd">Hello anh em!</h1>}</div>;
+    return <div>{<h1 className="">Hello anh em!</h1>}</div>;
 }
 
 export default App;
